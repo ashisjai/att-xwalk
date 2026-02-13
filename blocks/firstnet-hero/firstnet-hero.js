9dabde8c136a13fce9d3a61422fbb8431fbf7a48
FirstNet Migration <migration@firstnet.com>
2026-02-13 14:47:22 +0000

    Add 4 blocks
    Automated migration updates generated by Experience Modernization Agent.
    
    Changed files (6):
    - blocks/firstnet-cards-news/_firstnet-cards-news.json
    - blocks/firstnet-cards-offers/_firstnet-cards-offers.json
    - blocks/firstnet-form/_firstnet-form.json
    - blocks/firstnet-hero/_firstnet-hero.json
    - blocks/firstnet-hero/firstnet-hero.js
    - component-models.json

--- blocks/firstnet-hero/firstnet-hero.js
+++ blocks/firstnet-hero/firstnet-hero.js
@@ -1,4 +1,5 @@
 // FirstNet Hero block - no JS decoration needed (CSS-only block)
+// eslint-disable-next-line no-unused-vars
 export default function decorate(block) {
   // Hero block uses CSS-only decoration
 }
